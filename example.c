9e5151f273d59c1877f5554eecb8b596f3b47c2d
amiguel03 <148122874+amiguel03@users.noreply.github.com>
2024-01-12 12:28:36 +0100

    Add files via upload

---
+++ example.c
@@ -0,0 +1,43 @@
+#include "ft_printf.h"
+#include <limits.h>
+
+int main()
+{
+	// int value = 42; 
+	// char c;
+	// c = 'A';
+	// char *s;
+	// s = "Up to you";
+	// void *p;
+	// p = &value;
+	// char d;
+	// d = 42;
+	// int i;
+	// i = 23;
+	// char u;
+	// u = 43;
+	// char x;
+	// x = 's';
+	// char X;
+	// X = 'H';
+	// ft_printf ("%c\n", c);
+	// printf ("%c\n", c);
+	// ft_printf ("%s\n", s);
+	// printf ("%s\n", s);
+	// ft_printf ("%p\n", p);
+	// printf ("%p\n", p);
+	// ft_printf ("%d\n", d);
+	// printf ("%d\n", d);
+	// ft_printf ("%i\n", i);
+	// printf ("%i\n", i);
+	// ft_printf ("%u\n", u);
+	// printf ("%u\n", u);
+	// ft_printf ("%x\n", x);
+	// printf ("%x\n", x);
+	// ft_printf ("%X\n", X);
+	// printf ("%X\n", X);
+	// ft_printf ("%%\n");
+	// printf ("%%\n");
+	ft_printf("\t[%%%d]\n", INT_MAX);
+	return(0);
+}
